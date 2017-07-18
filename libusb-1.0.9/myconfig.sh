46fc074f3d298b81ee8b516628a32a6a36c1361e
jiangxiujie <jiangxiujie@cantv.cn>
2017-07-18 13:35:23 +0800

    libusb1.0.9编译通过在arm平台，myconfig.sh

---
+++ libusb-1.0.9/myconfig.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+MGCC=/opt/toolchain/mstar/linaro-aarch64_linux-2014.09_843419-patched/bin/aarch64-linux-gnu-gcc
+./configure CC=$MGCC  --host=arm-linux --prefix=$PWD/_install  
+make && make install
