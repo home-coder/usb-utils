6b4d2c3cff076ed2dae0d35ed93d95ea2a9367b7
jiangxiujie <jiangxiujie@cantv.cn>
2017-07-18 13:38:38 +0800

    x86平台编译

--- libusb-1.0.9/myconfig.sh
+++ libusb-1.0.9/myconfig.sh
@@ -2,3 +2,7 @@
 MGCC=/opt/toolchain/mstar/linaro-aarch64_linux-2014.09_843419-patched/bin/aarch64-linux-gnu-gcc
 ./configure CC=$MGCC  --host=arm-linux --prefix=$PWD/_install  
 make && make install
+
+#---------x86平台----------
+./configure  --prefix=$PWD/_install86
+make && make install
